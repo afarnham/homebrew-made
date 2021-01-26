9ae6b2fb3974114b8dd1771e33ed6c141afbad1f
Aaron Farnham <aaron@foreflight.com>
2021-01-26 11:14:10 -0600

    Made an arm64 bottle

--- Formula/xcodes.rb
+++ Formula/xcodes.rb
@@ -1,13 +1,13 @@
 class Xcodes < Formula
   desc "Manage the Xcodes installed on your Mac"
-  homepage "https://github.com/RobotsAndPencils/xcodes"
-  url "https://github.com/RobotsAndPencils/xcodes.git", tag: "0.15.0"
-  head "https://github.com/RobotsAndPencils/xcodes.git"
+  homepage "https://github.com/afarnham/xcodes"
+  url "https://github.com/afarnham/xcodes.git", tag: "0.15.1"
+  head "https://github.com/afarnham/xcodes.git"
 
   bottle do
-    root_url 'https://github.com/RobotsAndPencils/xcodes/releases/download/0.15.0'
+    root_url 'https://github.com/afarnham/xcodes/releases/download/0.15.1'
     cellar :any_skip_relocation
-    sha256 "48a3c95e144f9032a6c9319e2081a2a44842296f59b7e78600ebe4037e2bf42e" => :mojave
+    sha256 "3e8a2d322d25a0038fe41820dc3caf2a35d04325f715912cec3906bb916d8cea" => :arm64_big_sur
   end
 
   def install
